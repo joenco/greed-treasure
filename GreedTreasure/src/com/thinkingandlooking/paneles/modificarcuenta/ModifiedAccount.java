9ae1171756b16c48efab011d235a38b288922d1c
muchacho.julio@gmail.com <muchacho.julio@gmail.com@bfe274d5-1510-5a9a-36aa-4e3d1ba153ce>
2011-09-17 09:19:39 +0000

    Unos cambios de modificar contraseña

--- GreedTreasure/src/com/thinkingandlooking/paneles/modificarcuenta/ModifiedAccount.java
+++ GreedTreasure/src/com/thinkingandlooking/paneles/modificarcuenta/ModifiedAccount.java
@@ -1,10 +1,15 @@
 package com.thinkingandlooking.paneles.modificarcuenta;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.hibernate.Session;
 
 import com.thinkingandlooking.database.*;
+import com.thinkingandlooking.main.MainApp;
 import com.thinkingandlooking.utils.*;
 import nextapp.echo.app.Alignment;
+import nextapp.echo.app.ApplicationInstance;
 import nextapp.echo.app.Border;
 import nextapp.echo.app.Button;
 import nextapp.echo.app.Color;
@@ -14,11 +19,13 @@ import nextapp.echo.app.Grid;
 import nextapp.echo.app.Label;
 import nextapp.echo.app.Panel;
 import nextapp.echo.app.PasswordField;
+import nextapp.echo.app.SelectField;
 import nextapp.echo.app.TextField;
 import nextapp.echo.app.WindowPane;
 import nextapp.echo.app.event.ActionEvent;
 import nextapp.echo.app.event.ActionListener;
 import nextapp.echo.app.event.EventListenerList;
+import nextapp.echo.app.list.DefaultListModel;
 
 
 public class ModifiedAccount extends Panel {
@@ -36,13 +43,13 @@ public class ModifiedAccount extends Panel {
 	new ActionListenerProxy(eventListenerListOk);
 
 	private Usuario usuario;
-
+	String txtPaisS;
 	private TextField txtNombre;
 	private TextField txtApellido;
-	private TextField txtPais;
 	private PasswordField txtPass;
 	private TextField txtEmail;
-
+	private SelectField selectPais;
+	
 	public ModifiedAccount(Usuario usuario) {
 		this.usuario = usuario;
 		initGUI();
@@ -64,12 +71,19 @@ public class ModifiedAccount extends Panel {
 		grid.add(lblLastName);
 		grid.add(txtApellido);
 		
-		txtPais = new TextField();
-		txtPais.setText(usuario.getPais());
+		
+		
+		DefaultListModel listModel = new DefaultListModel(new String[] {
+				"Afganistan", "Africa del Sur", "Albania", "Alemania", "Andorra", "Angola", "Antillas Holandesas", "Arabia Saudita", "Argelia", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarusia", "Belgica", "Belice", "Benin", "Bermudas", "Bolivia", "Bosnia", "Brasil", "Bulgaria", "Burundi", "Butan", "Camboya", "Camerún", "Canada", "Chile", "China", "Chipre", "Colombia", "Congo", "Corea del Norte", "Corea del Sur", "Costa de Marfíl", "Costa Rica", "Croasia", "Cuba", "Dinamarca", "Dominica", "Ecuador", "Egipto", "El Salvador", "Emiratos Arabes Unidos", "Eslovenia", "España", "Estados Unidos", "Estonia", "Etiopía", "Filipinas", "Finlandia", "Francia", "Gambia", "Georgia", "Ghana", "Granada", "Grecia", "Groenlandia", "Guatemala", "Guinea", "Haití", "Holanda", "Honduras", "Hong Kong", "Hungría", "India", "Indonesia", "Irak", "Iran", "Irlanda", "Islandia", "Islas Virgenes (U.S.)", "Israel", "Italia", "Jamaica", "Japón", "Jordania", "Kenia", "Kuwait", "Kyrgyzstan", "Libia", "Lituania", "Luxemburgo", "Macedonia", "Madagascar", "Malasia", "Mali", "Malta", "Marruecos", "Martinica", "Mexico", "Mónaco", "Mongolia", "Nicaragua", "Nigeria", "Noruega", "Pakistan", "Palestina", "Panamá", "Paraguay", "Perú", "Polonia", "Portugal", "Puerto Rico", "Qatar", "Reino Unido", "Republica Checa", "Republica Democratica del Congo", "Republica Dominicana", "Republica Eslovaca", "Ruanda", "Rumania", "Rusia", "Sahara", "San Marino", "Santa Lucía", "Santa Sede (Vaticano)", "Senegal", "Singapur", "Siría", "Somalia", "Suecia", "Suiza", "Sur Africa", "Surinam", "Tailandia", "Taiwan", "Tanzania", "Togo", "Trinidad & Tobago", "Turquía", "Ucrania", "Uganda", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yugoslavia", "Zambia", "Zimbabwe" } );
+		
+		selectPais = new SelectField(listModel);
+		selectPais.setSelectedItem("Venezuela");
 		Label lblCountry = new Label("País");
 		grid.add(lblCountry);
-		grid.add(txtPais);
+		grid.add(selectPais);
 
+		
+		
 		txtPass = new PasswordField();
 		txtPass.setText(usuario.getPassword());
 		Label lblPass = new Label("Contraseña");
@@ -81,6 +95,8 @@ public class ModifiedAccount extends Panel {
 		Label lblEmail = new Label("Email");
 		grid.add(lblEmail);
 		grid.add(txtEmail);
+		
+		
 
 		Button btnModifiedAccount = new Button("Modificar Cuenta");
 		btnModifiedAccount.setBorder(new Border(new Extent(2), Color.BLACK, 1));
@@ -114,7 +130,11 @@ public class ModifiedAccount extends Panel {
 				windowPane.userClose();
 			}
 		});
-
+		
+		txtPaisS = new String();
+		txtPaisS = (String) selectPais.getSelectedItem();
+		
+		
 		Label lbl = new Label();
 
 		if (!validateFields()) {
@@ -129,7 +149,44 @@ public class ModifiedAccount extends Panel {
 			actionListenerProxyError.fireActionEvent(new ActionEvent(windowPane, null));
 			return;
 		}
+		/********************************************/
 		
+		
+		
+		Pattern pat = Pattern.compile("(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,10})$");
+        Matcher mat = pat.matcher(txtPass.getText());
+
+        if (!mat.find()) {
+       
+        	windowPane.setTitle("Contraseña no segura!!!");
+
+        	lbl.setText("La contraseña debe tener entre 6 y 10 caracteres, por lo menos un digito, un alfanumérico, y no puede contener caracteres especiales.");
+
+        	col.add(lbl);
+        	col.add(btnOK);
+        	windowPane.add(col);
+        	((MainApp)ApplicationInstance.getActive()).getDefaultWindow().getContent().add(windowPane);
+        	return;
+        }
+        
+		pat = Pattern.compile("^([0-9a-zA-Z]([_.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+([a-zA-Z]{2,9}.)+[a-zA-Z]{2,3})$");
+        mat = pat.matcher(txtEmail.getText());
+
+        if (!mat.find()) {
+        	windowPane.setTitle("Correo inválido!!");
+
+        	lbl.setText("Por favor escriba un correo válido.");
+
+        	col.add(lbl);
+        	col.add(btnOK);
+        	windowPane.add(col);
+        	((MainApp)ApplicationInstance.getActive()).getDefaultWindow().getContent().add(windowPane);
+        	return;
+        }
+		
+		
+		
+		/********************************************/		
 		modified();
 		
 		windowPane.setTitle("Cuenta Modificada");
@@ -151,7 +208,7 @@ public class ModifiedAccount extends Panel {
 		usuario.setPassword(txtPass.getText());
 		usuario.setNombre(txtNombre.getText());
 		usuario.setApellido(txtApellido.getText());
-		usuario.setPais(txtPais.getText());
+		usuario.setPais(txtPaisS);
 		usuario.setEmail(txtEmail.getText());
 		session.update(usuario);
 		
@@ -164,9 +221,6 @@ public class ModifiedAccount extends Panel {
 			return false;
 		}
 		if (txtApellido.getText().equals("")) {
-			return false;
-		}
-		if (txtPais.getText().equals("")) {
 			return false;
 		}
 		if (txtPass.getText().equals("")) {
